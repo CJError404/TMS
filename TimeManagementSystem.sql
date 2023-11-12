6eba33f0f599a3852e4811f8c8d615a5547260ce
CJError404 <102851896+CJError404@users.noreply.github.com>
2023-11-12 20:17:25 +0800

    Update TimeManagementSystem.sql

--- TimeManagementSystem.sql
+++ TimeManagementSystem.sql
@@ -1,16 +1,17 @@
--- create database
+-- create the database for the TMS
 CREATE DATABASE time_management_system;
 
--- create tables
+-- create the tasks table to store information about individual tasks
 CREATE TABLE tasks (
 task_id INT AUTO_INCREMENT PRIMARY KEY,
 title VARCHAR(255) NOT NULL,
 description TEXT,
-due_date VARCHAR(255),
+due_date DATE,
 priority INT,
 completed BOOLEAN DEFAULT 0
 );
 
+-- create the events table to store information about scheduled events
 CREATE TABLE events (
 event_id INT AUTO_INCREMENT PRIMARY KEY,
 title VARCHAR(255) NOT NULL,
@@ -18,6 +19,7 @@ description TEXT,
 date_time DATETIME
 );
 
+-- create the users table to store information about system users (admin or regular users)
 CREATE TABLE users (
 user_id INT AUTO_INCREMENT PRIMARY KEY,
 username VARCHAR(255) NOT NULL,
