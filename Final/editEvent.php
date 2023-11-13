043071772ae41881e5cb3d46dd9a49ed0307958d
Aishifishy <115207646+Aishifishy@users.noreply.github.com>
2023-11-14 03:06:22 +0800

    Update editEvent.php

--- Final/editEvent.php
+++ Final/editEvent.php
@@ -9,32 +9,44 @@ if (!$connection) {
     $errors[] = mysqli_connect_error();
 }
 
-$event_id = $_GET['event_id'];
-$query = "SELECT * FROM events_ WHERE event_id = ?";
-
 if (isset($_POST['updateForm'])) {
     $event_id = $_POST['event_id'];
     $title = $_POST['title'];
     $event_description = $_POST['event_description'];
     $date_time = $_POST['date_time'];
 
-    if (count($errors) == 0) {
-        $updateQuery = "UPDATE events_ SET title = ?, event_description = ?, date_time = ? WHERE event_id = ?";
-        $stmt = mysqli_prepare($connection, $updateQuery);
-        if ($stmt) {
-            mysqli_stmt_bind_param($stmt, "sssi", $title, $event_description, $date_time, $event_id);
-            if (!mysqli_stmt_execute($stmt)) {
-                $errors[] = mysqli_error($connection);
-            }
-        } else {
-            $errors[] = mysqli_error($connection);
-        }
+    $updateQuery = "UPDATE events_ SET title = ?, event_description = ?, date_time = ? WHERE event_id = ?";
+    $stmt = mysqli_prepare($connection, $updateQuery);
 
-        if (count($errors) == 0) {
+    if ($stmt) {
+        mysqli_stmt_bind_param($stmt, "sssi", $title, $event_description, $date_time, $event_id);
+
+        if (mysqli_stmt_execute($stmt)) {
             header('location: createEvent.php');
             exit();
+        } else {
+            $errors[] = "Error updating event: " . mysqli_error($connection);
         }
+    } else {
+        $errors[] = "Statement preparation error: " . mysqli_error($connection);
     }
+}
+
+// Fetch event details for the form
+$event_id = $_GET['event_id'];
+$query = "SELECT * FROM events_ WHERE event_id = ?";
+$stmt = mysqli_prepare($connection, $query);
+
+if ($stmt) {
+    mysqli_stmt_bind_param($stmt, "i", $event_id);
+    if (mysqli_stmt_execute($stmt)) {
+        $result = mysqli_stmt_get_result($stmt);
+        $row = mysqli_fetch_assoc($result);
+    } else {
+        $errors[] = "Error fetching event details: " . mysqli_error($connection);
+    }
+} else {
+    $errors[] = "Statement preparation error: " . mysqli_error($connection);
 }
 ?>
 
@@ -54,13 +66,11 @@ if (isset($_POST['updateForm'])) {
     </div>
 <?php endif; ?>
 <form action="editEvent.php?event_id=<?php echo $event_id; ?>" method="POST">
-    <?php while($row = mysqli_fetch_array($sql)): ?>
-        <input type="hidden" name="event_id" value="<?php echo $row['event_id']; ?>">
-        <input type="text" name="title" value="<?php echo $row['title']; ?>"> <br><br>
-        <input type="text" name="event_description" value="<?php echo $row['event_description']; ?>"><br><br>
-        <input type="text" name="date_time" value="<?php echo $row['date_time']; ?>"><br><br>
-        <input type="submit" name="updateForm" value="Update"><br><br>
-    <?php endwhile; ?>
+    <input type="hidden" name="event_id" value="<?php echo $row['event_id']; ?>">
+    <input type="text" name="title" value="<?php echo $row['title']; ?>"> <br><br>
+    <input type="text" name="event_description" value="<?php echo $row['event_description']; ?>"><br><br>
+    <input type="text" name="date_time" value="<?php echo $row['date_time']; ?>"><br><br>
+    <input type="submit" name="updateForm" value="Update"><br><br>
 </form>
 </body>
 </html>
