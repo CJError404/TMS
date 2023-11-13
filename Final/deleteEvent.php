de383e77ded58d73cd87eb96c6094c22607d34e6
kyle <150525689+sndaycndy@users.noreply.github.com>
2023-11-14 02:53:50 +0800

    Update deleteEvent.php

--- Final/deleteEvent.php
+++ Final/deleteEvent.php
@@ -2,10 +2,10 @@
       require 'config/connection.php';
       $connection = mysqli_connect(DB_HOST, DB_USER, PASSWORD, DB_NAME);
 
-      $task_id = $_GET['event_id'];
+      $event_id = $_GET['event_id'];
 
-      $deleteLogs = "DELETE FROM event_logs WHERE event_id = "$task_id"";
-      $deleteEvent = "DELETE FROM events WHERE event_id ='$event_id'";
+      $deleteLogs = "DELETE FROM event_logs WHERE event_id ='$event_id'";
+      $deleteEvent = "DELETE FROM events_ WHERE event_id ='$event_id'";
       
       $resultLogs = mysqli_query($connection, $deleteLogs);
       $resultEvent = mysqli_query($connection, $deleteEvent);
