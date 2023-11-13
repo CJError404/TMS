b236f30edc1e97bf977154a37e637d96a2414efa
Aishifishy <115207646+Aishifishy@users.noreply.github.com>
2023-11-14 00:28:15 +0800

    Update delete.php

--- update/delete.php
+++ update/delete.php
@@ -5,7 +5,7 @@
       $task_id = $_GET['task_id'];
 
       $deleteTask = "DELETE FROM tasks WHERE task_id ='$task_id'";
-      $deleteLogs = "DELETE FROM tasks_logs WHERE task_id = '$task_id'"
+      $deleteLogs = "DELETE FROM tasks_logs WHERE task_id = '$task_id'";
 
       $resultTasks = mysqli_query($connection, $deleteTask);
       $resultLogs = mysqli_query($connection, $deleteLogs);
@@ -16,6 +16,6 @@
            exit();
         }
       else{
-         echo ("Error Deleting task or task log") . mysqli_error($connection);
+         echo "Error Deleting task or task log" . mysqli_error($connection);
       }
 ?>
